7bbf434e1aef52245bf99ef2c68feeae86fb1f74
luisa <luisaheise3277@gmail.com>
2021-07-22 19:57:06 -0300

    morre next

--- next.config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    COGNITO_REGION: "sa-east-1",
-    COGNITO_CLIENT_ID: "2v8k236bs40rlj0t46sng39gap",
-    COGNITO_USER_POOL_ID: "sa-east-1_1AbQKRT1S"
-    }
-}
